e3e09d905279bc7eabc1bc4b982265e8f28f2f40
pajecawav <pajecawav@gmail.com>
2021-01-22 23:23:18 +0300

    Refactor extensions setup

--- backend/dmail/extensions/__init__.py
+++ backend/dmail/extensions/__init__.py
@@ -2,11 +2,15 @@ from typing import Dict
 
 from fastapi import APIRouter
 
-from . import telegram
-from .base import MessageHandlerBase
+from dmail.config import settings
+from dmail.extensions.base import MessageHandlerBase
 
 router = APIRouter()
 
-HANDLERS: Dict[str, MessageHandlerBase] = {telegram.EXTENSION_NAME: telegram.bot}
+HANDLERS: Dict[str, MessageHandlerBase] = {}
 
-router.include_router(telegram.router, prefix="/telegram", include_in_schema=False)
+if settings.TELEGRAM_BOT_TOKEN:
+    from . import telegram
+
+    HANDLERS[telegram.EXTENSION_NAME] = telegram.bot
+    router.include_router(telegram.router, prefix="/telegram", include_in_schema=False)
